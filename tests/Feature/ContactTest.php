5b28b4daf9612d86b2a12de037bd15867e2fa297
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-04-03 20:37:47 +0200

    test env

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_fetch_contacts()
+    {
+//        $user = factory()
+    }
+}
